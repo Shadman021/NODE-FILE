843754f3fd9d84e05903b05019d8eabfb9d44ff0
First-3-6-25-demo <shadman12278@gmail.com>
2025-08-01 17:44:28 +0530

    Lecture 8 completed

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-require("./xyz")
-const calculatesum = require("./sum");
-var a=10;
-var b=20;
-console.log(a+b);
-console.log(globalThis == global);
-calculatesum();
+
+const crypto = require("node:crypto");
+var a=1073698;
+var b=209056;
+
+console.log("program started");
+
+
+crypto.pbkdf2Sync("password","salt", 50000, 50, "sha512");
+console.log("first key is generated.");
+console.log(crypto);
+
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512",(err, key) =>{
+    console.log("second key is generated");  
+})
+
+function multiplyfn(x,y){
+    const result = a*b;
+    return result;
+}
+console.log("multipyly result: ",multiplyfn(a,b));
+
+
